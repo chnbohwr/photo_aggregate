83b14c2eabcb9f35f2dd88077f04838bab5d169f
chnbohwr <chnbohwr@gmail.com>
2017-11-04 15:33:22 +0800

    use react transition group

--- src/containers/PhotoAnimate/PhotoAnimate.js
+++ src/containers/PhotoAnimate/PhotoAnimate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Transition from 'react-transition-group/Transition';
+import { Transition, TransitionGroup } from 'react-transition-group';
 import inside from 'point-in-polygon';
+import Random from 'random-js';
 import path from './path.json';
 
 import './PhotoAnimate.less';
@@ -13,23 +14,51 @@ export default class PhotoAnimate extends Component {
     randomMaxY: 620,
   }
 
-  constructor() {
-    super();
-    this.state = {
-      leftPhotos: Array.from(new Array(this.config.seeds)).map(() => this.generatePosition(path.pathLeft)),
-      rightPhotos: Array.from(new Array(this.config.seeds)).map(() => this.generatePosition(path.pathRight)),
+  state = {
+    leftPhotos: [],
+    rightPhotos: []
+  }
+
+  createPhoto = (pathPoints) => {
+    let x1 = 0; let y1 = 0; let x2 = 0; let y2 = 0;
+    const uuid = Random.uuid4(Random.engines.nativeMath);
+    while (!inside([x1, y1], pathPoints)) {
+      x1 = Random.integer(0, this.config.randomMaxX)(Random.engines.nativeMath);
+      y1 = Random.integer(0, this.config.randomMaxY)(Random.engines.nativeMath);
+    }
+    x2 = Random.integer(-300, 500)(Random.engines.nativeMath);
+    y2 = Random.integer(-300, 700)(Random.engines.nativeMath);
+    const transitionStyle = {
+      entering: { transform: `translate(${x2}px, ${y2}px)`, opacity: 0 },
+      entered: { transform: `translate(${x1}px, ${y1}px)`, opacity: 1 },
+      exiting: { opacity: 1 },
+      edited: { opacity: 0 }
     };
+    return (
+      <Transition timeout={10} key={uuid}>
+        {state => <img className="photo" style={transitionStyle[state]} alt="goodimg" src="http://fakeimg.pl/100" />}
+      </Transition>
+    );
   }
 
 
-  generatePosition = (pathPoints) => {
-    let x = 0;
-    let y = 0;
-    while (!inside([x, y], pathPoints)) {
-      x = Math.floor(Math.random() * this.config.randomMaxX);
-      y = Math.floor(Math.random() * this.config.randomMaxY);
-    }
-    return <img className="photo" alt="goodimg" src="http://fakeimg.pl/100" style={{ transform: `translate(${x}px, ${y}px)` }} />;
+  qqq = () => {
+    const photo = this.createPhoto(path.pathLeft);
+    this.setState({
+      leftPhotos: [...this.state.leftPhotos.slice(-200), photo],
+    });
+  }
+
+  rrr = () => {
+    const photo = this.createPhoto(path.pathRight);
+    this.setState({
+      rightPhotos: [...this.state.rightPhotos.slice(-200), photo],
+    });
+  }
+
+  componentDidMount() {
+    setInterval(this.qqq, 510);
+    setInterval(this.rrr, 520);
   }
 
   render() {
@@ -37,10 +66,14 @@ export default class PhotoAnimate extends Component {
       <div id="photoAnimate">
         <div id="container">
           <div id="leftSide">
-            {this.state.leftPhotos}
+            <TransitionGroup>
+              {this.state.leftPhotos}
+            </TransitionGroup>
           </div>
           <div id="rightSide">
-            {this.state.rightPhotos}
+            <TransitionGroup>
+              {this.state.rightPhotos}
+            </TransitionGroup>
           </div>
         </div>
       </div>
